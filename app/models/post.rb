2da50cadc561b5b800cb2dfbf6a37c8d0da140ba
Ben Kielty <bwk103@gmail.com>
2018-02-12 16:12:54 +0000

    Added picture column to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
 
   validates :caption, length: { maximum: 2200 }
+  mount_uploader :picture, PictureUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212140852) do
+ActiveRecord::Schema.define(version: 20180212160039) do
 
   create_table "posts", force: :cascade do |t|
     t.text "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
 end
