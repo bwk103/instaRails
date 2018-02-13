b7e35e0ca019351b602727c7e8fab8f4765111e3
Ben Kielty <bwk103@gmail.com>
2018-02-13 07:56:06 +0000

    Add further detail to front end
    Added further detail to posts view.  Added seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create!(
+  picture: File.open(Rails.root + "app/assets/images/seed_images/1.jpg"),
+  caption: 'The coffee here is out of this world!'
+)
+
+Post.create!(
+  picture: File.open(Rails.root + "app/assets/images/seed_images/2.jpg"),
+  caption: 'Stag Stag Stag'
+)
+
+Post.create!(
+  picture: File.open(Rails.root + "app/assets/images/seed_images/3.jpg"),
+  caption: 'You stay classy NYC'
+)
+
+Post.create!(
+  picture: File.open(Rails.root + "app/assets/images/seed_images/4.jpg"),
+  caption: 'Around the world at Epcot!'
+)
+
+Post.create!(
+  picture: File.open(Rails.root + "app/assets/images/seed_images/5.jpg"),
+  caption: 'I feel the same way on a Monday morning'
+)
