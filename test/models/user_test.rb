45640ad61c9ab9c29f9c7056f51935ab7f27fbf9
Ben Kielty <bwk103@gmail.com>
2018-02-14 11:46:59 +0000

    Add Validators to User Password
    Added presence and minimum length validators to User passwords.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,29 +2,23 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test 'name is unique' do
-    User.create!(name: 'Bob', email: 'bob@test.com')
-    @user = User.new
-    @user.name = 'Bob'
-    @user.email = 'bob@test.com'
-    assert_no_difference 'User.count' do
-      @user.save
-    end
-    assert_not @user.errors.empty?
+  def setup
+    @user = User.new(name: 'Bob', email: 'bob@test.com', password: 'testing',
+                  password_confirmation: 'testing')
   end
 
   test 'name and email are required' do
-    @user = User.new
+    @user.name = ''
     assert_not @user.valid?
     @user.name = 'Bob'
+    assert @user.valid?
+    @user.email = ''
     assert_not @user.valid?
     @user.email = 'bob@test.com'
     assert @user.valid?
   end
 
   test 'email requires necessary format' do
-    @user = User.new
-    @user.name = 'Bob'
     @user.email = 'not_an_email'
     assert_not @user.valid?
     @user.email = 'bob@test.com'
@@ -32,14 +26,27 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'email is unique' do
-    User.create!(name: 'Bob', email: 'bob@test.com')
-    @user = User.new
-    @user.name = 'Bill'
-    @user.email = 'bob@test.com'
+    @user.save
+    @user2 = User.new(name: 'Bill', email: 'bob@test.com', password: 'testing',
+                    password_confirmation: 'testing')
     assert_no_difference 'User.count' do
-      @user.save
+      @user2.save
     end
-    assert_not @user.errors.empty?
   end
 
+  test 'password is required' do
+    @user.password = ''
+    @user.password_confirmation = ''
+    assert_not @user.valid?
+  end
+
+  test 'password cannot be blank' do
+    @user.password = @user.password_confirmation = ' ' * 5
+    assert_not @user.valid?
+  end
+
+  test 'password should have a minimum length of 5' do
+    @user.password = @user.password_confirmation = 'test'
+    assert_not @user.valid?
+  end
 end
