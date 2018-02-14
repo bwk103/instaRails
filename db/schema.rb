0d2956232a6b50265a1818a3a0bdff526c571402
Ben Kielty <bwk103@gmail.com>
2018-02-14 10:14:18 +0000

    Add User Model
    Created User model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212160039) do
+ActiveRecord::Schema.define(version: 20180214101104) do
 
   create_table "posts", force: :cascade do |t|
     t.text "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.string "confirm_password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
