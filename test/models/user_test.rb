02e6c790fdc0fb863ea5fb7c528e2aba1654719f
Ben Kielty <bwk103@gmail.com>
2018-02-14 10:46:25 +0000

    Add validators to User
    Added presence and uniqueness validators to user model.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'name is unique' do
+    User.create!(name: 'Bob', email: 'bob@test.com')
+    @user = User.new
+    @user.name = 'Bob'
+    @user.email = 'bob@test.com'
+    assert_no_difference 'User.count' do
+      @user.save
+    end
+    assert_not @user.errors.empty?
+  end
+
+  test 'name and email are required' do
+    @user = User.new
+    assert_not @user.valid?
+    @user.name = 'Bob'
+    assert_not @user.valid?
+    @user.email = 'bob@test.com'
+    assert @user.valid?
+  end
+
+  test 'email is unique' do
+    User.create!(name: 'Bob', email: 'bob@test.com')
+    @user = User.new
+    @user.name = 'Bill'
+    @user.email = 'bob@test.com'
+    assert_no_difference 'User.count' do
+      @user.save
+    end
+    assert_not @user.errors.empty?
+  end
+
 end
