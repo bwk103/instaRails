dd87f3f322539294b6d6d0fbe1ad19f2cb330af2
Ben Kielty <bwk103@gmail.com>
2018-02-19 12:44:24 +0000

    Add Users#edit
    Added edit method to User controller.  Refactored User/new form into
    _user_form and rendered same form in edit route.  Added unit tests in
    User controller.  Added checks that user is logged in and is correct
    user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit]
+  before_action :correct_user, only: [:edit]
 
   def new
     @user = User.new
@@ -19,12 +21,30 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'users/edit'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :username, :email, :profile, :password,
                                  :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless @user == current_user
   end
 
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,6 +5,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = User.create!(name: 'Bob', username: 'bobby', email: 'bob@test.com',
                  password: 'password', password_confirmation: 'password')
+    @user2 = users(:james)
   end
 
   test 'invalid login' do
@@ -19,5 +20,43 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     post login_path, params: {session: { username: 'bobby',
                                          password: 'password'}}
     assert_redirected_to user_path(@user)
+  end
+
+  test 'redirected from edit if user not logged in' do
+    get edit_user_path(@user.id)
+    assert_redirected_to login_url
+  end
+
+  test 'redirected from edit if user is not correct user' do
+    log_in_as(@user)
+    get edit_user_path(@user2.id)
+    assert_redirected_to root_url
+  end
+
+  test 'user can access edit page if logged in as correct user' do
+    log_in_as(@user)
+    get edit_user_path(@user.id)
+    assert_template 'users/edit'
+  end
+
+  test 'invalid edits are redirected to edit page' do
+    log_in_as(@user)
+    get edit_user_path(@user.id)
+    patch user_path, params: {user: { name: '', username: '',
+                                           email: '', password: '',
+                                           password_confirmation: ''}}
+    assert_template 'users/edit'
+  end
+
+  test 'valid edits are redirected to profile page' do
+    log_in_as(@user)
+    get edit_user_path(@user.id)
+    patch user_path, params: {user: { name: 'Bob Smith',
+                                      username: @user.username,
+                                      email: @user.email,
+                                      password: @user.password,
+                            password_confirmation: @user.password_confirmation
+                            }}
+    assert_redirected_to @user
   end
 end
