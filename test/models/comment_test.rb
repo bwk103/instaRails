c4d0cc9aae2c54a05c9b5abbfb05d74aa7f6b379
Ben Kielty <bwk103@gmail.com>
2018-02-20 08:42:21 +0000

    Added presence, length validators for Comment Model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @comment = Comment.new(content: 'This is a test comment')
+  end
+
+  test "should be valid" do
+    assert @comment.valid?
+  end
+
+  test 'content is required' do
+    @comment.content = ''
+    assert_not @comment.valid?
+  end
+
+  test 'content cannot be longer than 300 characters' do
+    @comment.content = 'a' * 301
+    assert_not @comment.valid?
+  end
 end
