f6d0b3a02444f736f3f6dbc600ed0d5201cca71d
Ben Kielty <bwk103@gmail.com>
2018-02-20 08:37:36 +0000

    Created basic Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216155902) do
+ActiveRecord::Schema.define(version: 20180220083647) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "caption"
