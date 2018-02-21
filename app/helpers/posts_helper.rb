960cfa4601fa2d8b63b08bef93d92dd42b1647e7
Ben Kielty <bwk103@gmail.com>
2018-02-21 13:25:17 +0000

    Added js response to user likes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,4 +3,13 @@ module PostsHelper
   def is_author?(post_id)
     current_user == Post.find(post_id).user
   end
+
+  def liked_by(post)
+    votes = post.votes_for.up.by_type(User)
+    pluralize(votes.count, 'like')
+  end
+
+  def liked_post(post)
+    current_user.voted_for? post
+  end
 end
