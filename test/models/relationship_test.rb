796d6ea3f7d0e236183ca4c0279594f0058f3b4b
Ben Kielty <bwk103@gmail.com>
2018-02-24 14:08:26 +0000

    Added presence validators to relationships

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @relationship = Relationship.new(follower_id: users(:james).id,
+                                     followed_id: users(:mike).id)
+  end
+
+  test 'is valid' do
+    assert @relationship.valid?
+  end
+
+  test 'requires a valid follower_id' do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test 'requires a valid followed_id' do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
