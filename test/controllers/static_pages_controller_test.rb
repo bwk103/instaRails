7dc9d4765f25e4896c2ddfabaae766c03afb1b65
Ben Kielty <bwk103@gmail.com>
2018-02-26 14:32:30 +0000

    Added style to landing page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,9 +12,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "root should be home" do
     get root_path
-    assert_select "h1", text: "Welcome to instaRails"
-    assert_select "p", text: "Sign up to start sharing your photos!"
-    assert_select "a[href=?]", login_path, count: 1
+    assert_select "h1", text: "instaRails"
+    assert_select "h4", text: "share your joy"
+    assert_select "a[href=?]", login_path, count: 2
     assert_select "a[href=?]", signup_path, count: 2
     assert_select "title", text: "Home | instaRails"
   end
